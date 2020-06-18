789df93aca346b054f1797d42d9c0ad2ab28cfe4
Abdullah Bin Asad <abdullahbinasad14@gmail.com>
2020-06-17 22:20:36 -0400

    created sql_scripts for table creation

--- Milestone_1/data/createTables.sql
+++ Milestone_1/data/createTables.sql
@@ -1,39 +1,73 @@
-CREATE TABLE Crime
-  ( 
-    crimeID  INT NOT NULL PRIMARY KEY, 
-    offence    VARCHAR(50), 
-    MCI    VARCHAR(10)
-  ); 
-
-CREATE TABLE LocationTable 
-  ( 
-    locationID         INT NOT NULL PRIMARY KEY, 
-     premisetype       VARCHAR(10), 
-     division          VARCHAR(3), 
-     hoodID            INT,
-     neighbourhood     VARCHAR(30),  
-     long              DECIMAL(9, 7), 
-     lat               DECIMAL(9, 7), 
-  ); 
-
-CREATE TABLE TimeTable 
-  ( 
-    timeID                   INT NOT NULL PRIMARY KEY, 
-     occurrenceHour          INT,
-     occurrenceDay           INT,
-     occurrenceMonth         VARCHAR(20), 
-     occurrenceYear          INT, 
-     occurrenceDayOfWeek     VARCHAR(30),  
-
-  ); 
-
-CREATE TABLE enrolled 
-  ( 
-     eventUniqueId  VARCHAR(14) NOT NULL PRIMARY KEY, 
-     crimeID        INT
-     locationID     INT
-     timeID         INT
-     FOREIGN KEY(crimeID) REFERENCES Crime(crimeID), 
-     FOREIGN KEY(locationID) REFERENCES LocationTable(locationID), 
-     FOREIGN KEY(timeID) REFERENCES TimeTable(timeID), 
-  ); 
+-- connect to CS348;
+create TABLE Time (
+  time_id INT NOT NULL PRIMARY KEY,
+  hour INT NOT NULL,
+  day INT NOT NULL,
+  month INT NOT NULL,
+  year INT NOT NULL,
+  day_of_week INT NOT NULL
+);
+create TABLE Neighbourhood (
+  hood_id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  division INT NOT NULL
+);
+create TABLE RegularCrime(
+  crime_id INT NOT NULL PRIMARY KEY,
+  offence VARCHAR(10) NOT NULL,
+  MCI VARCHAR(10) NOT NULL
+);
+create TABLE BikeTheft(
+  bike_theft_id INT NOT NULL PRIMARY KEY,
+  colour VARCHAR(15) NOT NULL,
+  make VARCHAR(15) NOT NULL,
+  model VARCHAR(15) NOT NULL,
+  speed VARCHAR(15) NOT NULL,
+  bike_type VARCHAR(15) NOT NULL,
+  status VARCHAR(15) NOT NULL,
+  cost FLOAT NOT NULL
+);
+create TABLE CrimeEvent (
+  event_id INT NOT NULL PRIMARY KEY,
+  occurrence_time_id INT NOT NULL,
+  reported_time_id INT,
+  crime_id INT,
+  bike_theft_id INT,
+  hood_id INT NOT NULL,
+  lat FLOAT NOT NULL,
+  long FLOAT NOT NULL,
+  premise_type VARCHAR(50),
+  FOREIGN KEY(occurrence_time_id) REFERENCES Time(time_id),
+  FOREIGN KEY(reported_time_id) REFERENCES Time(time_id),
+  FOREIGN KEY(crime_id) REFERENCES RegularCrime(crime_id),
+  FOREIGN KEY(bike_theft_id) REFERENCES BikeTheft(bike_theft_id),
+  FOREIGN KEY(hood_id) REFERENCES Neighbourhood(hood_id)
+);
+create TABLE InvolvedPerson(
+  accident_id INT NOT NULL,
+  person_id INT NOT NULL,
+  PRIMARY KEY(accident_id, person_id),
+  involvement_type VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  injury VARCHAR(50) NOT NULL,
+  vehicle_type VARCHAR(10),
+  action_taken VARCHAR(50)
+);
+create TABLE RoadCondition(
+  road_condition_id INT NOT NULL PRIMARY KEY,
+  classification VARCHAR(10) NOT NULL,
+  traffic_control_type VARCHAR(10),
+  visibility VARCHAR(10) NOT NULL,
+  surface_condition VARCHAR(10) NOT NULL
+);
+create TABLE TrafficEvent(
+  accident_id INT NOT NULL PRIMARY KEY,
+  occurrence_time_id INT NOT NULL,
+  road_condition_id INT NOT NULL,
+  hood_id INT NOT NULL,
+  lat FLOAT NOT NULL,
+  long FLOAT NOT NULL,
+  FOREIGN KEY(occurrence_time_id) REFERENCES Time(time_id),
+  FOREIGN KEY(road_condition_id) REFERENCES RoadCondition(road_condition_id),
+  FOREIGN KEY(hood_id) REFERENCES Neighbourhood(hood_id)
+);
---
+++ Milestone_1/data/dropTables.sql
@@ -0,0 +1,8 @@
+DROP TABLE Time;
+DROP TABLE Neighbourhood;
+DROP TABLE RegularCrime;
+DROP TABLE BikeTheft;
+DROP TABLE CrimeEvent;
+DROP TABLE InvolvedPerson;
+DROP TABLE RoadCondition;
+DROP TABLE TrafficEvent;
