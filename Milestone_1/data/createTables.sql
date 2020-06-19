4dbb6b4f235c06d5a1803f7ecec60cf127684d93
LukmanMohamed <lukmanmohamed857@gmail.com>
2020-06-18 21:18:22 -0400

    Removed all non-primary key NOT NULL's.

--- Milestone_1/data/createTables.sql
+++ Milestone_1/data/createTables.sql
@@ -1,41 +1,41 @@
 -- connect to CS348;
 create TABLE IncidentTime (
   time_id INT NOT NULL PRIMARY KEY,
-  hour INT NOT NULL,
-  day INT NOT NULL,
-  month INT NOT NULL,
-  year INT NOT NULL,
-  day_of_week INT NOT NULL
+  hour INT,
+  day INT,
+  month INT,
+  year INT,
+  day_of_week INT
 );
 create TABLE Neighbourhood (
   hood_id INT NOT NULL PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  division INT NOT NULL
+  name VARCHAR(50),
+  division INT
 );
 create TABLE RegularCrime(
   crime_id INT NOT NULL PRIMARY KEY,
-  offence VARCHAR(10) NOT NULL,
-  MCI VARCHAR(10) NOT NULL
+  offence VARCHAR(10),
+  MCI VARCHAR(10)
 );
 create TABLE BikeTheft(
   bike_theft_id INT NOT NULL PRIMARY KEY,
-  colour VARCHAR(15) NOT NULL,
-  make VARCHAR(15) NOT NULL,
-  model VARCHAR(15) NOT NULL,
-  speed VARCHAR(15) NOT NULL,
-  bike_type VARCHAR(15) NOT NULL,
-  status VARCHAR(15) NOT NULL,
-  cost FLOAT NOT NULL
+  colour VARCHAR(15),
+  make VARCHAR(15),
+  model VARCHAR(15),
+  speed VARCHAR(15),
+  bike_type VARCHAR(15),
+  status VARCHAR(15),
+  cost FLOAT
 );
 create TABLE CrimeEvent (
   event_id INT NOT NULL PRIMARY KEY,
-  occurrence_time_id INT NOT NULL,
+  occurrence_time_id INT,
   reported_time_id INT,
   crime_id INT,
   bike_theft_id INT,
-  hood_id INT NOT NULL,
-  lat FLOAT NOT NULL,
-  long FLOAT NOT NULL,
+  hood_id INT,
+  lat FLOAT,
+  long FLOAT,
   premise_type VARCHAR(50),
   FOREIGN KEY(occurrence_time_id) REFERENCES IncidentTime(time_id),
   FOREIGN KEY(reported_time_id) REFERENCES IncidentTime(time_id),
@@ -47,26 +47,26 @@ create TABLE InvolvedPerson(
   accident_id INT NOT NULL,
   person_id INT NOT NULL,
   PRIMARY KEY(accident_id, person_id),
-  involvement_type VARCHAR(50) NOT NULL,
-  age INT NOT NULL,
-  injury VARCHAR(50) NOT NULL,
+  involvement_type VARCHAR(50),
+  age INT,
+  injury VARCHAR(50),
   vehicle_type VARCHAR(10),
   action_taken VARCHAR(50)
 );
 create TABLE RoadCondition(
   road_condition_id INT NOT NULL PRIMARY KEY,
-  classification VARCHAR(10) NOT NULL,
+  classification VARCHAR(10),
   traffic_control_type VARCHAR(10),
-  visibility VARCHAR(10) NOT NULL,
-  surface_condition VARCHAR(10) NOT NULL
+  visibility VARCHAR(10),
+  surface_condition VARCHAR(10)
 );
 create TABLE TrafficEvent(
   accident_id INT NOT NULL PRIMARY KEY,
-  occurrence_time_id INT NOT NULL,
-  road_condition_id INT NOT NULL,
-  hood_id INT NOT NULL,
-  lat FLOAT NOT NULL,
-  long FLOAT NOT NULL,
+  occurrence_time_id INT,
+  road_condition_id INT,
+  hood_id INT,
+  lat FLOAT,
+  long FLOAT,
   FOREIGN KEY(occurrence_time_id) REFERENCES IncidentTime(time_id),
   FOREIGN KEY(road_condition_id) REFERENCES RoadCondition(road_condition_id),
   FOREIGN KEY(hood_id) REFERENCES Neighbourhood(hood_id)
