203f6b1e2b9726a8143ba0a4f012dd6ff73f965d
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-06-20 11:42:12 -0600

    add component library

--- Milestone_1/watch-dog/client/src/App.js
+++ Milestone_1/watch-dog/client/src/App.js
@@ -1,10 +1,24 @@
 import React, {useState} from 'react';
+import {Header, Container, Menu} from 'semantic-ui-react';
+
 import './App.css';
 
 function App () {
   return (
     <div>
-      <p>Hello</p>
+      <Menu fixed="top" inverted>
+        <Container>
+          <Menu.Item as="a" header>
+            Project Name
+          </Menu.Item>
+          <Menu.Item as="a">Home</Menu.Item>
+        </Container>
+      </Menu>
+      <Container style={{marginTop: '3em'}}>
+        <Header as="h1s">
+          WatchDog
+        </Header>
+      </Container>
     </div>
   );
 }
