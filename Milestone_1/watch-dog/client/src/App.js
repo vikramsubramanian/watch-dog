22245dcfd680de70c5edf1715400a5c5a76c880c
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-06-20 11:14:56 -0600

    clean up template code

--- Milestone_1/watch-dog/client/src/App.js
+++ Milestone_1/watch-dog/client/src/App.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello</p>
     </div>
   );
 }
