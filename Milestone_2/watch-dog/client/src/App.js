e2605bf6a422c8462e7387cc4c6662889df05976
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-07-12 17:15:03 -0600

    add toasts

--- Milestone_2/watch-dog/client/src/App.js
+++ Milestone_2/watch-dog/client/src/App.js
@@ -8,6 +8,8 @@ import {
   Segment,
   Grid,
 } from 'semantic-ui-react';
+import {SemanticToastContainer, toast} from 'react-semantic-toasts';
+import 'react-semantic-toasts/styles/react-semantic-alert.css';
 
 import './App.css';
 
@@ -32,28 +34,57 @@ function App () {
     if (!strEqual (crimeIndicator, 'all')) {
       path += '&MCI=' + crimeIndicator;
     }
-    fetch (path).then (response => response.json ()).then (data => {
-      console.log (data);
-      setDateType (dateType);
-      setCrimeData (data);
-      setCards ([
-        {
-          src: <TableCard crimeData={data} dateType={dateType} />,
-          group: 0,
-          width: null,
-        },
-        {
-          src: <LineChart crimeData={data} dateType={dateType} />,
-          group: 1,
-          width: null,
-        },
-        {
-          src: <TextCard data={FINE_PRINT} />,
-          group: 1,
-          width: 4,
-        },
-      ]);
-    });
+    fetch (path)
+      .then (response => response.json ())
+      .then (data => {
+        toast (
+          {
+            title: 'Success',
+            description: <p>Fetched data!</p>,
+            type: 'success',
+            time: 2500,
+            animation: 'fade left',
+          },
+          () => console.log ('toast closed'),
+          () => console.log ('toast clicked'),
+          () => console.log ('toast dismissed')
+        );
+        console.log (data);
+        setDateType (dateType);
+        setCrimeData (data);
+        setCards ([
+          {
+            src: <TableCard crimeData={data} dateType={dateType} />,
+            group: 0,
+            width: null,
+          },
+          {
+            src: <LineChart crimeData={data} dateType={dateType} />,
+            group: 1,
+            width: null,
+          },
+          {
+            src: <TextCard data={FINE_PRINT} />,
+            group: 1,
+            width: 4,
+          },
+        ]);
+      })
+      .catch (error => {
+        console.error ('Error:', error);
+        toast (
+          {
+            title: 'Error: DB',
+            description: <p>Could not connect to DB.</p>,
+            type: 'error',
+            time: 2500,
+            animation: 'fade left',
+          },
+          () => console.log ('toast closed'),
+          () => console.log ('toast clicked'),
+          () => console.log ('toast dismissed')
+        );
+      });
   }
 
   useEffect (() => {
@@ -93,7 +124,7 @@ function App () {
           })}
         </Grid>
       </Container>
-
+      <SemanticToastContainer />
     </div>
   );
 }
