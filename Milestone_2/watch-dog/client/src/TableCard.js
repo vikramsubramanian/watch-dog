19aec2febba6e01ca1f888917837cbcc4997f082
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-07-13 00:42:51 -0600

    clean up

--- Milestone_2/watch-dog/client/src/TableCard.js
+++ Milestone_2/watch-dog/client/src/TableCard.js
@@ -8,17 +8,6 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 import './TableCard.css';
 
-const columnDefs = [
-  {headerName: 'Make', field: 'make'},
-  {headerName: 'Model', field: 'model'},
-  {headerName: 'Price', field: 'price'},
-];
-const rowData = [
-  {make: 'Toyota', model: 'Celica', price: 35000},
-  {make: 'Ford', model: 'Mondeo', price: 32000},
-  {make: 'Porsche', model: 'Boxter', price: 72000},
-];
-
 function TableCard (props) {
   const [columnDefs, setColumnsDefs] = useState ([]);
   const [activeIndex, setActiveIndex] = useState (-1);
