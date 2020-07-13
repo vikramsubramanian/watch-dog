c8cf4206376bf3bd91e462f8e19825c9f2d1bcc4
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-07-13 00:00:38 -0600

    collapse table

--- Milestone_2/watch-dog/client/src/TableCard.js
+++ Milestone_2/watch-dog/client/src/TableCard.js
@@ -1,8 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import {Table} from 'semantic-ui-react';
+import {Label, Button, Accordion, Icon} from 'semantic-ui-react';
+
+import './TableCard.css';
 
 function TableCard (props) {
   const [columns, setColumns] = useState ([]);
+  const [activeIndex, setActiveIndex] = useState (-1);
 
   useEffect (
     () => {
@@ -14,28 +18,55 @@ function TableCard (props) {
     [props.crimeData]
   );
 
-  return (
-    <Table striped>
-      <Table.Header>
-        <Table.Row>
-          {columns.map ((col, ind) => {
-            return <Table.HeaderCell key={ind}>{col}</Table.HeaderCell>;
-          })}
-        </Table.Row>
-      </Table.Header>
+  function handleClick (e, titleProps) {
+    const {index} = titleProps;
+    const newIndex = activeIndex === index ? -1 : index;
 
-      <Table.Body>
-        {props.crimeData.map ((crime, ind) => {
-          return (
-            <Table.Row key={ind}>
-              {columns.map (col => {
-                return <Table.Cell>{crime[col]}</Table.Cell>;
+    setActiveIndex (newIndex);
+  }
+
+  return (
+    <div>
+      <Accordion>
+        <Accordion.Title
+          active={activeIndex === 0}
+          index={0}
+          onClick={handleClick}
+        >
+          <div className="tableWrapper">
+            <Icon name="dropdown" />
+            <h2 className="wrapperHeader">See a detailed table of these</h2>
+            <Label color={'blue'} className="wrapperTotal">
+              {props.crimeData.length}
+            </Label>
+            <h2 className="wrapperHeader">incidents</h2>
+          </div>
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+          <Table striped>
+            <Table.Header>
+              <Table.Row>
+                {columns.map ((col, ind) => {
+                  return <Table.HeaderCell key={ind}>{col}</Table.HeaderCell>;
+                })}
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {props.crimeData.map ((crime, ind) => {
+                return (
+                  <Table.Row key={ind}>
+                    {columns.map (col => {
+                      return <Table.Cell>{crime[col]}</Table.Cell>;
+                    })}
+                  </Table.Row>
+                );
               })}
-            </Table.Row>
-          );
-        })}
-      </Table.Body>
-    </Table>
+            </Table.Body>
+          </Table>
+        </Accordion.Content>
+      </Accordion>
+    </div>
   );
 }
 
