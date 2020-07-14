f53b8b16041158ce6136d8d49c967b21832ce54c
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-07-14 00:01:17 -0600

    add date picker; neighbour query

--- Milestone_2/watch-dog/client/src/ReportCrime.js
+++ Milestone_2/watch-dog/client/src/ReportCrime.js
@@ -1,51 +1,90 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect, useRef} from 'react';
 import {Button, Header, Icon, Modal, Form} from 'semantic-ui-react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-const options = [
-  {key: 'm', text: 'Male', value: 'male'},
-  {key: 'f', text: 'Female', value: 'female'},
+const premiseTypeOptions = [
+  {key: 'h', text: 'House', value: 'House'},
+  {key: 'c', text: 'Commercial', value: 'Commercial'},
   {key: 'o', text: 'Other', value: 'other'},
 ];
 
 function ReportCrime () {
   const [modalOpen, setModalOpen] = useState (false);
-  const [value, setValue] = useState ('');
+  const [startDate, setStartDate] = useState (new Date ().setMinutes (0));
+  const [hoodOptions, setHoodOptions] = useState ([]);
+
+  useEffect (() => {
+    var allHoods = [];
+    fetch ('/neighbourhoods')
+      .then (response => response.json ())
+      .then (res => {
+        console.log (res);
+        res.forEach (hood => {
+          allHoods.push ({
+            key: hood['hood_id'],
+            text: hood['name'],
+            value: hood['name'],
+          });
+        });
+        setHoodOptions (allHoods);
+      })
+      .catch (err => {
+        console.log (err);
+      });
+  }, []);
 
   return (
     <div>
       <Modal open={modalOpen} onClose={() => setModalOpen (false)}>
         <Modal.Header>Report a Crime</Modal.Header>
-        <Modal.Content scrolling>
+        <Modal.Content>
 
           <Modal.Description>
             <Form>
-              <Form.Group widths="equal">
-                <Form.Input fluid label="First name" placeholder="First name" />
-                <Form.Input fluid label="Last name" placeholder="Last name" />
-                <Form.Select
-                  fluid
-                  label="Gender"
-                  options={options}
-                  placeholder="Gender"
+              <Form.Group inline>
+                <label>Occurence Date and Time</label>
+                <DatePicker
+                  selected={startDate}
+                  onChange={date => setStartDate (date)}
+                  showTimeSelect
+                  timeIntervals={60}
+                  timeFormat="HH:mm"
+                  dateFormat="MMMM d, yyyy h:mm aa"
                 />
               </Form.Group>
               <Form.Group inline>
-                <label>Size</label>
-                <Form.Radio label="Small" value="sm" checked={value === 'sm'} />
-                <Form.Radio
-                  label="Medium"
-                  value="md"
-                  checked={value === 'md'}
+                <label>Reported Date and Time</label>
+                <DatePicker
+                  selected={startDate}
+                  onChange={date => setStartDate (date)}
+                  showTimeSelect
+                  timeIntervals={60}
+                  timeFormat="HH:mm"
+                  dateFormat="MMMM d, yyyy h:mm aa"
                 />
-                <Form.Radio label="Large" value="lg" checked={value === 'lg'} />
               </Form.Group>
-              <Form.TextArea
-                label="About"
-                placeholder="Tell us more about you..."
-              />
+              <Form.Group widths="equal">
+                <Form.Select
+                  fluid
+                  label="Neighbourhood"
+                  options={hoodOptions}
+                  placeholder="Neighbourhood"
+                />
+              </Form.Group>
+              <Form.Group widths="equal">
+                <Form.Input fluid label="Latitude" placeholder="Latitude" />
+                <Form.Input fluid label="Longitude" placeholder="Longitude" />
+                <Form.Select
+                  fluid
+                  label="Premise Type"
+                  options={premiseTypeOptions}
+                  placeholder="Premise Type"
+                />
+              </Form.Group>
+
               <Form.Checkbox label="I agree to the Terms and Conditions" />
-              <Form.Button>Submit</Form.Button>
             </Form>
           </Modal.Description>
         </Modal.Content>
