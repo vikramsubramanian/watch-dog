c5a950da9228dad53b4e4d039ae2d9950f0833e9
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-07-15 12:54:06 -0600

    fix data issues during upload and create dump for backup

---
+++ Milestone_2/watch-dog/createIndexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX YearMonth ON IncidentTime(year, month);
+
+CREATE INDEX InvolvedPersonAge ON InvolvedPerson(age);
--- Milestone_2/watch-dog/createTables.sql
+++ Milestone_2/watch-dog/createTables.sql
@@ -8,24 +8,23 @@ create TABLE IncidentTime
   day_of_week INT CHECK(day_of_week IS NULL OR day_of_week >= 1 AND day_of_week <= 7)
 );
 
-
 create TABLE PoliceDivision
 (
   division INT NOT NULL PRIMARY KEY,
   address VARCHAR(50),
-  area FLOAT CHECK(area >= 0),
-  shapeLeng FLOAT CHECK(shapeLeng >= 0),
-  shapeArea FLOAT CHECK(shapeArea >= 0)
+  area DECIMAL(12, 9) CHECK(area >= 0),
+  shapeLeng DECIMAL(12, 6) CHECK(shapeLeng >= 0),
+  shapeArea DECIMAL(11, 3) CHECK(shapeArea >= 0)
 );
 
 create TABLE Neighbourhood
 (
   hood_id INT NOT NULL PRIMARY KEY,
   name VARCHAR(50),
-  employement_rate FLOAT CHECK(employement_rate >= 0),
-  high_school FLOAT CHECK(high_school >= 0),
-  university FLOAT CHECK(university >= 0),
-  technical_degree FLOAT CHECK(technical_degree >= 0),
+  employment_rate DECIMAL(4, 2) CHECK(employment_rate >= 0),
+  high_school DECIMAL(4, 2) CHECK(high_school >= 0),
+  university DECIMAL(4, 2) CHECK(university >= 0),
+  technical_degree DECIMAL(4, 2) CHECK(technical_degree >= 0),
   division INT,
   FOREIGN KEY(division) REFERENCES PoliceDivision(division)
 );
@@ -44,8 +43,8 @@ create TABLE CrimeEvent
   reported_time_id INT,
   crime_id INT,
   hood_id INT,
-  latitude FLOAT CHECK (latitude >= -90.0 AND latitude <= 90.0),
-  longitude FLOAT CHECK (longitude >= -180.0 AND longitude <= 180.0),
+  latitude DECIMAL(10,8) CHECK (latitude >= -90.0 AND latitude <= 90.0),
+  longitude DECIMAL(11,8) CHECK (longitude >= -180.0 AND longitude <= 180.0),
   premise_type VARCHAR(50),
   FOREIGN KEY(occurrence_time_id) REFERENCES IncidentTime(time_id),
   FOREIGN KEY(reported_time_id) REFERENCES IncidentTime(time_id),
@@ -56,13 +55,13 @@ create TABLE CrimeEvent
 create TABLE BikeTheft
 (
   event_id INT NOT NULL PRIMARY KEY,
-  colour VARCHAR(15),
-  make VARCHAR(15),
-  model VARCHAR(15),
-  speed VARCHAR(15),
-  bike_type VARCHAR(15),
-  status VARCHAR(15),
-  cost FLOAT CHECK (cost >= 0),
+  colour VARCHAR(50),
+  make VARCHAR(50),
+  model VARCHAR(50),
+  speed VARCHAR(50),
+  bike_type VARCHAR(50),
+  status VARCHAR(50),
+  cost DECIMAL(8, 2) CHECK (cost >= 0),
   FOREIGN KEY(event_id) REFERENCES CrimeEvent(event_id)
 );
 
@@ -81,8 +80,8 @@ create TABLE TrafficEvent
   occurrence_time_id INT,
   road_condition_id INT,
   hood_id INT,
-  latitude FLOAT CHECK (latitude >= -90.0 AND latitude <= 90.0),
-  longitude FLOAT CHECK (longitude >= -180.0 AND longitude <= 180.0),
+  latitude DECIMAL(10,8) CHECK (latitude >= -90.0 AND latitude <= 90.0),
+  longitude DECIMAL(11,8) CHECK (longitude >= -180.0 AND longitude <= 180.0),
   FOREIGN KEY(occurrence_time_id) REFERENCES IncidentTime(time_id),
   FOREIGN KEY(road_condition_id) REFERENCES RoadCondition(road_condition_id),
   FOREIGN KEY(hood_id) REFERENCES Neighbourhood(hood_id)
@@ -100,7 +99,3 @@ create TABLE InvolvedPerson
   action_taken VARCHAR(50),
   FOREIGN KEY(accident_id) REFERENCES TrafficEvent(accident_id)
 );
-
-CREATE INDEX YearMonth ON IncidentTime(year, month);
-
-CREATE INDEX InvolvedPersonAge ON InvolvedPerson(age);
